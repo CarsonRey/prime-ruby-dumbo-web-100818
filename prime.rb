fea14f2290275ab59179a83de57ab3027b5d85b3
Carson Rey <c.rey494@gmail.com>
2018-09-16 03:02:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -10,5 +10,6 @@ else
 end
 end
 
-#check that 'n' is a positive fixnum
+#check that 'n' is a positive fixnum / that it isn't equal to zero (or one)
+#check that the remainder 
 # make sure all numbers follow some condition
