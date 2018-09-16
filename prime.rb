d184ed52f54cccf834f111037fe0d7a6acae71fd
Carson Rey <c.rey494@gmail.com>
2018-09-16 02:51:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+  range = (2..n - 1)
+  range = range.to_a
+if n < 0
+  false
+elsif range.all? {|num| n % num == 0} 
+  true
+else
+  false
+end
+end
+
+#check that 'n' is a positive fixnum
+# make sure all numbers follow some condition
